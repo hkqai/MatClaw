98052a7d85d4b47e394f72fe4d099044aeb8be34
Sean Lau <seanlaush1020@gmail.com>
2026-04-20 11:27:45 +0800

    Added matcalc_calc_eos and matcalc_calc_phonon tools

--- mcp/tools/matcalc/__init__.py
+++ mcp/tools/matcalc/__init__.py
@@ -6,7 +6,9 @@ and transport properties from ML potentials or DFT calculations.
 """
 
 from .matcalc_calc_elasticity import matcalc_calc_elasticity
+from .matcalc_calc_eos import matcalc_calc_eos
 
 __all__ = [
     "matcalc_calc_elasticity",
+    "matcalc_calc_eos",
 ]
